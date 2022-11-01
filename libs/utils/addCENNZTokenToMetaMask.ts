c8b97917a17ef53b5ee4482df18d552f2ec9b7b5
Bharathcoorg <40591495+Bharathcoorg@users.noreply.github.com>
2022-11-02 00:27:18 +0530

    Update addCENNZTokenToMetaMask.ts

--- libs/utils/addCENNZTokenToMetaMask.ts
+++ libs/utils/addCENNZTokenToMetaMask.ts
@@ -7,9 +7,9 @@ export default async function addCENNZTokenToMetamask() {
 			type: "ERC20",
 			options: {
 				address: "0xcCccccCc00003E80000000000000000000000000",
-				symbol: "CENNZ",
-				decimals: 4,
-				image: CENNZ_IPFS,
+				symbol: "EDG",
+				decimals: 18,
+				image: EDG_IPFS,
 			},
 		},
 	});
