e43f7c4b49952717daea975e284b470cba1dcd8a
Bharathcoorg <40591495+Bharathcoorg@users.noreply.github.com>
2022-11-02 00:24:47 +0530

    Update constants.ts

--- libs/constants.ts
+++ libs/constants.ts
@@ -1,17 +1,17 @@
-import { CENNZnetToken, MetamaskNetworks } from "@/libs/types";
+import { EDGToken, MetamaskNetworks } from "@/libs/types";
 
-export const SUPPORTED_TOKENS: CENNZnetToken[] = [
+export const SUPPORTED_TOKENS: EDGToken[] = [
 	{
-		symbol: "CENNZ",
-		assetId: 16000,
+		symbol: "EDG",
+		assetId: 0,
 		logo: "cennz.svg",
-		decimals: 4,
+		decimals: 18,
 	},
 	{
-		symbol: "CPAY",
-		assetId: 16001,
+		symbol: "KAB",
+		assetId: 0,
 		logo: "cpay.svg",
-		decimals: 4,
+		decimals: 18,
 	},
 ];
 
@@ -30,11 +30,11 @@ export const TRANSFER_AMOUNT: number = Number(
 );
 
 export const CENNZNET_NIKAU_API_URL: string = String(
-	process.env.NEXT_PUBLIC_CENNZNET_NIKAU_API_URL
+	process.env.NEXT_PUBLIC_EDGEVERSE_BERESHEET_API_URL
 );
 
 export const CENNZNET_RATA_API_URL: string = String(
-	process.env.NEXT_PUBLIC_CENNZNET_RATA_API_URL
+	process.env.NEXT_PUBLIC_EDGEVERSE_SOUPCAN_API_URL
 );
 
 export const REDIS_URL: string = String(process.env.REDIS_URL);
@@ -43,23 +43,17 @@ export const GA_ID: string = process.env.NEXT_PUBLIC_GA_ID;
 
 export const NETWORKS: MetamaskNetworks = {
 	Nikau: {
-		blockExplorerUrl: "https://nikau.uncoverexplorer.com",
-		chainId: "0xbb9",
-		chainName: "CENNZnet Nikau",
-		rpcUrl: "https://nikau.centrality.me/public",
-	},
-	Rata: {
-		blockExplorerUrl: "https://rata.uncoverexplorer.com",
-		chainId: "0xbb8",
-		chainName: "CENNZnet Rata",
-		rpcUrl: "https://rata.centrality.me/public",
+		blockExplorerUrl: "https://edgscan.live",
+		chainId: "0x7e6",
+		chainName: "Edgeware EVM",
+		rpcUrl: "https://edgeware.api.onfinality.io/public",
 	},
 };
 
-export const CPAY_IPFS: string =
+export const EDG_IPFS: string =
 	"https://gateway.pinata.cloud/ipfs/QmehPHqUocYho8FLC2Hs9EgU4vgm698br6RwMEddw1m9VD";
 
-export const CENNZ_IPFS: string =
+export const KAB_IPFS: string =
 	"https://gateway.pinata.cloud/ipfs/QmfDkgrhCFfVJErVVDuU7UYasYsooXMEXFhBzLMNm6pgey";
 
 export const APP_VERSION: string = process.env.APP_VERSION;
