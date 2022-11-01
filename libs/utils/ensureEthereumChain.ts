db4eb16072cbe565c82caebbb1b0dcc3b85d7834
Bharathcoorg <40591495+Bharathcoorg@users.noreply.github.com>
2022-11-02 00:33:32 +0530

    Update ensureEthereumChain.ts

--- libs/utils/ensureEthereumChain.ts
+++ libs/utils/ensureEthereumChain.ts
@@ -1,15 +1,15 @@
 import { MetaMaskInpageProvider } from "@metamask/providers";
-import { CENNZnetNetwork } from "@/libs/types";
+import { EdgewareNetwork } from "@/libs/types";
 import { NETWORKS } from "@/libs/constants";
-import { addCENNZnetToMetaMask } from "@/libs/utils";
+import { addEDGToMetaMask } from "@/libs/utils";
 
 export default async function ensureEthereumChain(
 	extension: MetaMaskInpageProvider,
-	network: CENNZnetNetwork
+	network: EdgewareNetwork
 ): Promise<void> {
 	const ethChainId = await extension.request({ method: "eth_chainId" });
 
 	if (ethChainId === NETWORKS[network].chainId) return;
 
-	await addCENNZnetToMetaMask(extension, network);
+	await addEDGToMetaMask(extension, network);
 }
